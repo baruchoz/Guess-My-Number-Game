f049806527e3f7322a920d3283231b817efd0b9c
Baruch Rotbart <baruchrotbart@Baruchs-MacBook-Pro.local>
2022-04-17 12:45:57 -0500

    cleaned up js

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 'use strict';
-/*
-console.log(document.querySelector('.message').textContent); 
-document.querySelector('.message').textContent = 'Correct Number!';
-
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 20;
-
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
-*/
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
@@ -49,32 +39,9 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
-  /* else if (guess > secretNumber) {
-        if (score > 1) {
-            document.querySelector('.message').textContent = '📈 Too High!';
-            score--;
-            document.querySelector('.score').textContent = score;
-        } else  {
-            document.querySelector('.message').textContent = '💥 You lost the game!';
-            document.querySelector('.score').textContent = 0;
-        }
-
-        // When guess is too low
-    } else if (guess < secretNumber) {
-        if (score > 1) {
-            document.querySelector('.message').textContent = '📉 Too Low!';
-            score--;
-            document.querySelector('.score').textContent = score;
-        } else  {
-            document.querySelector('.message').textContent = '💥 You lost the game!';
-            document.querySelector('.score').textContent = 0;
-        }
-    }
-    */
 });
 
 ///////////////////////////////////////////////////////
-// Coding Challenge 1
 document.querySelector('.again').addEventListener('click', function () {
   //
   score = 20;
